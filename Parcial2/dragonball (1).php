eb48f000ee0560366d7c362c32d016fb11c1a526
Naoohw <katyanaomiflorese22.ce84@dgeti.sems.gob.mx>
2024-10-18 08:39:20 -0600

    Tarea Formatos

---
+++ Parcial2/dragonball (1).php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `dragonball`
+ */
+
+/* `dragonball`.`characters` */
+$characters = array(
+  array('id' => '1','user_id' => '1','name' => 'LegHo','race' => 'Saiyan','level' => '5','strength' => '61','speed' => '61','endurance' => '61','experience' => '0','avatar' => 'AVATAR7.jpeg'),
+  array('id' => '2','user_id' => '1','name' => 'Ghunn','race' => 'Humano','level' => '1','strength' => '50','speed' => '50','endurance' => '50','experience' => '0','avatar' => 'AVATAR3.jpeg')
+);
+
+/* `dragonball`.`character_transformations` */
+$character_transformations = array(
+);
+
+/* `dragonball`.`tournaments` */
+$tournaments = array(
+);
+
+/* `dragonball`.`tournament_participants` */
+$tournament_participants = array(
+);
+
+/* `dragonball`.`transformations` */
+$transformations = array(
+);
+
+/* `dragonball`.`users` */
+$users = array(
+  array('id' => '1','name' => 'Nao','email' => 'naomi@gmail.com','password' => '$2y$10$nVGVVABdbG/ZYhs81SbUseaNDYCT8VXCT.OebmWvyH9EpyOqAQh6q','avatar' => 'AVATAR8.jpeg'),
+  array('id' => '2','name' => 'Val','email' => 'ValVal@gmail.com','password' => '$2y$10$REdcROvt0PBxVVASEyGYU.SAv/AvvUnySNOdCctaMEQrgx9YC2yNu','avatar' => 'AVATAR8'),
+  array('id' => '3','name' => 'Tadeo','email' => 'tedd90@gmail.com','password' => '$2y$10$YGphrlQhvBEOQuj3V7i2POnRLDzgyc2gGlJ00bbbVLZjHi4869U6q','avatar' => 'AVATAR9.png')
+);
